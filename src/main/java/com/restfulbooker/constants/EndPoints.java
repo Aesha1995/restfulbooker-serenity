091ff294c2670d14ec1a78a9550ac990db414743
Aesha1995 <aeshap2905@gmail.com>
2023-06-06 23:11:56 +0100

    restfulbooker-serenity homework

---
+++ src/main/java/com/restfulbooker/constants/EndPoints.java
@@ -0,0 +1,17 @@
+package com.restfulbooker.constants;
+
+/**
+ * Created by Mamta
+ */
+public class EndPoints {
+    /**
+     * This is Endpoints of Booking section
+     */
+
+    public static final String GET_ALL_BOOKINGS_BY_NAME = "?firstname=";
+    public static final String GET_ALL_BOOKINGS = "";
+    public static final String GET_SINGLE_USER_BY_ID = "/{bookingID}";
+    public static final String UPDATE_USER_BY_ID = "/{bookingID}";
+    public static final String DELETE_USER_BY_ID = "/{bookingID}";
+
+}
---
+++ src/main/java/com/restfulbooker/constants/Path.java
@@ -0,0 +1,16 @@
+package com.restfulbooker.constants;
+
+/**
+ * Created by Mamta
+ */
+public class Path {
+    /*
+     * This is base path for user api
+     */
+    public static final String BOOKING = "/booking";
+
+    /*
+     * This is base path for auth api
+     */
+    public static final String AUTH = "/auth";
+}
